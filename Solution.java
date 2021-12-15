3d55594d8bb721e9ad23c4b07411262214a947cf
Monica <monicakrishna29@gmail.com>
2021-12-15 23:10:54 +0530

    First commit

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String s;
+        Scanner in = new Scanner(System.in);
+        int i =1;
+        while(in.hasNext()){
+            s=in.nextLine();
+            System.out.println(i+" "+s);
+            i++;
+        }
+        
+    }
+}
